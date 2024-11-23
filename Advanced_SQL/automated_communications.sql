a3d89e05f8e1a716c43620ae44b47ffc3db90f86
Isaac Ngugi <117882836+iamisaackn@users.noreply.github.com>
2024-11-23 12:52:00 +0300

    Update automated_communications.sql

---
+++ Advanced_SQL/automated_communications.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `automated_communications` (
+    `Communication_id` int NOT NULL AUTO_INCREMENT,
+    `Client_id` int NOT NULL,
+    `Communication_date` datetime NOT NULL,
+    `Message` text NOT NULL,
+    `Status` varchar(50) NOT NULL,
+    PRIMARY KEY (`Communication_id`),
+    FOREIGN KEY (`Client_id`) REFERENCES `clients`(`Client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
